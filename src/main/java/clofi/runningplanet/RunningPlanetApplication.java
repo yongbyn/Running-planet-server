afe2f08764527731c294d499a3c82251ecaee5a1
ZENO <zeno1030@naver.com>
2024-05-14 15:00:47 +0900

    init setting

---
+++ src/main/java/clofi/runningplanet/RunningPlanetApplication.java
@@ -0,0 +1,13 @@
+package clofi.runningplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningPlanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clofi/runningplanet/RunningPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package clofi.runningplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
