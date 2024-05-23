aa50bdf8b3bc7d04806742662a4be84013d23c0e
Lee <94841127+galaxian@users.noreply.github.com>
2024-05-23 15:13:40 +0900

    feat: 크루 신청 기능 controller 구현

--- src/main/java/clofi/runningplanet/crew/controller/CrewController.java
+++ src/main/java/clofi/runningplanet/crew/controller/CrewController.java
@@ -4,16 +4,20 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import clofi.runningplanet.crew.dto.request.ApplyCrewReqDto;
 import clofi.runningplanet.crew.dto.request.CreateCrewReqDto;
+import clofi.runningplanet.crew.dto.response.ApplyCrewResDto;
 import clofi.runningplanet.crew.dto.response.FindAllCrewResDto;
 import clofi.runningplanet.crew.dto.response.FindCrewResDto;
 import clofi.runningplanet.crew.service.CrewService;
+import clofi.runningplanet.member.dto.CustomOAuth2User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -37,5 +41,11 @@ public class CrewController {
 	@GetMapping("/api/crew/{crewId}")
 	public ResponseEntity<FindCrewResDto> findCrew(@PathVariable Long crewId) {
 		return ResponseEntity.ok(crewService.findCrew(crewId));
+	}
+
+	@PostMapping("/api/crew/{crewId}")
+	public ResponseEntity<ApplyCrewResDto> applyCrew(@PathVariable Long crewId, @RequestBody ApplyCrewReqDto reqDto,
+		@AuthenticationPrincipal CustomOAuth2User principal) {
+		return ResponseEntity.ok(crewService.applyCrew(reqDto, crewId, principal.getName()));
 	}
 }
