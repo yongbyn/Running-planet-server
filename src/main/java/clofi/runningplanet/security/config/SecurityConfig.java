4b98f65ff0a172f7a0c921a72c3ac70805bfdcce
YongBin Kim <yongbyn@naver.com>
2024-05-24 15:33:59 +0900

    feat. CORS 관련 쿠키 설정 및 유저 검증 관련 ID 추가

--- src/main/java/clofi/runningplanet/security/config/SecurityConfig.java
+++ src/main/java/clofi/runningplanet/security/config/SecurityConfig.java
@@ -84,8 +84,8 @@ public class SecurityConfig {
 					configuration.setAllowedHeaders(Collections.singletonList("*"));
 					configuration.setMaxAge(3600L);
 
-					configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+					configuration.setExposedHeaders(List.of("Set-Cookie","Authorization"));
+
 
 					return configuration;
 				}
