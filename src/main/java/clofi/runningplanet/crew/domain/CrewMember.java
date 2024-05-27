9c67751181150b6412b4a64f23112cd6435371e6
YongBin Kim <yongbyn@naver.com>
2024-05-27 11:57:23 +0900

    fix. 테스트 수정

--- src/main/java/clofi/runningplanet/crew/domain/CrewMember.java
+++ src/main/java/clofi/runningplanet/crew/domain/CrewMember.java
@@ -17,6 +17,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
