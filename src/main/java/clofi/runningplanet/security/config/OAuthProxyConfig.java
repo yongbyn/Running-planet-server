7d225710590b479a4c3671d75d2999f3c8e17c55
hlmg <devhlmg@gmail.com>
2024-05-30 11:27:10 +0900

    feat: OAuth 외부 호출에 프록시 적용

---
+++ src/main/java/clofi/runningplanet/security/config/OAuthProxyConfig.java
@@ -0,0 +1,31 @@
+package clofi.runningplanet.security.config;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class OAuthProxyConfig {
+
+	@Bean
+	public DefaultAuthorizationCodeTokenResponseClient authorizationCodeAccessTokenResponseClient() {
+		DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient =
+			new DefaultAuthorizationCodeTokenResponseClient();
+		accessTokenResponseClient.setRestOperations(proxyRestTemplate());
+
+		return accessTokenResponseClient;
+	}
+
+	@Bean
+	public RestTemplate proxyRestTemplate() {
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("http://krmp-proxy.9rum.cc", 3128));
+		factory.setProxy(proxy);
+		return new RestTemplate(factory);
+	}
+}
