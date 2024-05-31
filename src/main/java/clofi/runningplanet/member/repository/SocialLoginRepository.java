b13d4f71c71a1693616b26a29c610c299156bc01
YongBin Kim <yongbyn@naver.com>
2024-05-31 15:13:24 +0900

    SocialLogin find시 Member fetch join 적용

--- src/main/java/clofi/runningplanet/member/repository/SocialLoginRepository.java
+++ src/main/java/clofi/runningplanet/member/repository/SocialLoginRepository.java
@@ -1,6 +1,8 @@
 package clofi.runningplanet.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import clofi.runningplanet.member.domain.OAuthType;
 import clofi.runningplanet.member.domain.SocialLogin;
@@ -9,6 +11,7 @@ public interface SocialLoginRepository extends JpaRepository<SocialLogin, Long> 
 
 	Boolean existsByOauthTypeAndOauthId(OAuthType oauthType, String oAuthId);
 
-	SocialLogin findByOauthTypeAndOauthId(OAuthType oauthType, String oAuthId);
+	@Query("SELECT sl FROM SocialLogin sl JOIN FETCH sl.member WHERE sl.oauthType = :oauthType AND sl.oauthId = :oauthId")
+	SocialLogin findByOauthTypeAndOauthIdWithMember(@Param("oauthType") OAuthType oauthType,@Param("oauthId") String oauthId);
 
 }
