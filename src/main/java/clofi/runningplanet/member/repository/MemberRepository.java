673111fce35abac3dcbb7fcb329eafa4878464c0
YongBin Kim <yongbyn@naver.com>
2024-06-07 18:26:32 +0900

    feat. 유저 검증을 위해 반환값 Optional로 수정

--- src/main/java/clofi/runningplanet/member/repository/MemberRepository.java
+++ src/main/java/clofi/runningplanet/member/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package clofi.runningplanet.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import clofi.runningplanet.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-	Member findByNickname(String nickName);
+	Optional<Member> findByNickname(String nickName);
 }
