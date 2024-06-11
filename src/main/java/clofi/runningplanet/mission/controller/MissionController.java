318437ccca60c0952a58d13a186e1c538e9a13d9
Lee <94841127+galaxian@users.noreply.github.com>
2024-06-11 20:46:29 +0900

    feat: 크루 미션 성공 컨트롤러 구현

--- src/main/java/clofi/runningplanet/mission/controller/MissionController.java
+++ src/main/java/clofi/runningplanet/mission/controller/MissionController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import clofi.runningplanet.member.dto.CustomOAuth2User;
@@ -22,5 +23,12 @@ public class MissionController {
 		@AuthenticationPrincipal
 		CustomOAuth2User principal) {
 		return ResponseEntity.ok(missionService.getCrewMission(crewId, principal.getId()));
+	}
+
+	@PostMapping("api/crew/{crewId}/mission/{missionId}")
+	public ResponseEntity<Void> successMission(@PathVariable("crewId") Long crewId,
+		@PathVariable("missionId") Long missionId, @AuthenticationPrincipal CustomOAuth2User principal) {
+		missionService.successMission(crewId, missionId, principal.getId());
+		return ResponseEntity.ok().build();
 	}
 }
