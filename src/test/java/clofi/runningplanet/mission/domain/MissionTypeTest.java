8b3bc7c6aef262e85796a16b1fa497256310c8ef
Lee <94841127+galaxian@users.noreply.github.com>
2024-06-11 21:12:45 +0900

    test: 3600초 미만으로 운동했을 시 false 반환 테스트

--- src/test/java/clofi/runningplanet/mission/domain/MissionTypeTest.java
+++ src/test/java/clofi/runningplanet/mission/domain/MissionTypeTest.java
@@ -10,10 +10,10 @@ import clofi.runningplanet.mission.domain.vo.TodayRecords;
 
 class MissionTypeTest {
 
-	@DisplayName("3600초 이상 운동했을 시 미션 성공 판단 로직 성공")
+	@DisplayName("3600초 이상 운동했을 시 true 반환")
 	@ParameterizedTest
 	@ValueSource(ints = {3600, 3601, 4000, 10000})
-	void successIsComplete(int value) {
+	void durationIsCompleteReturnTrue(int value) {
 		//given
 		MissionType type = MissionType.DURATION;
 		TodayRecords records = new TodayRecords(500, value);
@@ -23,5 +23,20 @@ class MissionTypeTest {
 
 		//then
 		assertThat(result).isTrue();
+	}
+
+	@DisplayName("3600초 미만으로 운동했을 시 false 반환")
+	@ParameterizedTest
+	@ValueSource(ints = {0, 1, 2000, 3599})
+	void durationIsCompleteReturnFalse(int value) {
+		//given
+		MissionType type = MissionType.DURATION;
+		TodayRecords records = new TodayRecords(500, value);
+
+		//when
+		boolean result = type.isComplete(records);
+
+		//then
+		assertThat(result).isFalse();
 	}
 }
