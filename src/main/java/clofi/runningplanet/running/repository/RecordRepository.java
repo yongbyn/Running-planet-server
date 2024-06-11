0451e053e8c38054cfef1d8ef2e0fb83b0ac4d2d
hlmg <devhlmg@gmail.com>
2024-06-12 03:41:18 +0900

    refactor: 날짜 조건 between 사용하게 변경

--- src/main/java/clofi/runningplanet/running/repository/RecordRepository.java
+++ src/main/java/clofi/runningplanet/running/repository/RecordRepository.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import clofi.runningplanet.member.domain.Member;
 import clofi.runningplanet.running.domain.Record;
@@ -19,15 +17,8 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 
 	List<Record> findAllByMemberIdAndCreatedAtBetween(Long memberId, LocalDateTime start, LocalDateTime end);
 
-	List<Record> findAllByMemberAndCreatedAtBetweenAndEndTimeIsNotNull(Member member, LocalDateTime startDateTime,
-		LocalDateTime endDateTime);
+	List<Record> findAllByMemberAndCreatedAtBetweenAndEndTimeIsNotNull(Member member, LocalDateTime start,
+		LocalDateTime end);
 
-	@Query("SELECT r FROM Record r WHERE r.member = :member AND r.createdAt >= :startOfToday AND r.createdAt < :startOfTomorrow")
-	List<Record> findRunningRecordsByMemberAndDateRange(@Param("member") Member member,
-		@Param("startOfToday") LocalDateTime startOfToday, @Param("startOfTomorrow") LocalDateTime startOfTomorrow);
-
-	@Query("SELECT r FROM Record r WHERE r.member IN :members AND r.createdAt >= :startOfToday AND r.createdAt < :startOfTomorrow")
-	List<Record> findRunningRecordsByMembersAndDateRange(@Param("members") List<Member> members,
-		@Param("startOfToday") LocalDateTime startOfToday, @Param("startOfTomorrow") LocalDateTime startOfTomorrow);
-
+	List<Record> findAllByMemberInAndCreatedAtBetween(List<Member> members, LocalDateTime start, LocalDateTime end);
 }
