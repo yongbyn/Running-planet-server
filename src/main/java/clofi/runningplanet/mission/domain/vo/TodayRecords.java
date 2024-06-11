cdd1331ca192441a6882f2e61eb46db993060492
Lee <94841127+galaxian@users.noreply.github.com>
2024-06-11 20:42:31 +0900

    feat: TodayRecords 클래스 내부 클래스에서 vo 변경

---
+++ src/main/java/clofi/runningplanet/mission/domain/vo/TodayRecords.java
@@ -0,0 +1,17 @@
+package clofi.runningplanet.mission.domain.vo;
+
+import java.util.List;
+
+import clofi.runningplanet.running.domain.Record;
+import lombok.Getter;
+
+@Getter
+public class TodayRecords {
+	private final double totalDistance;
+	private final int totalDuration;
+
+	public TodayRecords(List<Record> records) {
+		this.totalDistance = records.stream().mapToDouble(Record::getRunDistance).sum();
+		this.totalDuration = records.stream().mapToInt(Record::getRunTime).sum();
+	}
+}
