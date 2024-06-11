f9810e1b0c52a283ee539470c899b2c055c24abe
YongBin Kim <yongbyn@naver.com>
2024-06-11 16:32:54 +0900

    feat. Page method respository에 작성

--- src/main/java/clofi/runningplanet/crew/repository/ChatRepository.java
+++
@@ -1,8 +0,0 @@
-package clofi.runningplanet.crew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import clofi.runningplanet.crew.domain.Chat;
-
-public interface ChatRepository extends JpaRepository<Chat, Long> {
-}
