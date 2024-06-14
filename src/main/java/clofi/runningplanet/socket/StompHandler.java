6f76434f4c528e62cb28f69b4d4d9fb6e63129ee
Lee <94841127+galaxian@users.noreply.github.com>
2024-06-14 18:07:05 +0900

    fix: 소켓 헤더 접근 방법 수정
    MessageHeaderAccessor.getAccessor -> StompHeaderAccessor.wrap

--- src/main/java/clofi/runningplanet/socket/StompHandler.java
+++ src/main/java/clofi/runningplanet/socket/StompHandler.java
@@ -5,7 +5,6 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -28,10 +27,7 @@ public class StompHandler implements ChannelInterceptor {
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
-		StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-		if (accessor == null) {
-			throw new IllegalArgumentException("HeaderAccessor is null");
-		}
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
 		if (StompCommand.CONNECT.equals(accessor.getCommand())) {
 			String token = extractToken(accessor.getFirstNativeHeader(AUTHORIZATION_HEADER));
