9ba8e02db218508af009300844b22d3174e6002e
YongBin Kim <yongbyn@naver.com>
2024-07-04 20:57:23 +0900

    fix. MemberController login method 수정

--- src/main/java/clofi/runningplanet/member/controller/MemberController.java
+++ src/main/java/clofi/runningplanet/member/controller/MemberController.java
@@ -30,9 +30,15 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	@GetMapping("/api/kakaologin")
-	public void kakaoLogin(HttpServletResponse response) throws IOException {
-		response.sendRedirect("/oauth2/authorization/kakao");
+	@GetMapping("/api/login/{provider}")
+	public void login(@PathVariable("provider") String provider, HttpServletResponse response) throws IOException {
+		String redirectUrl = switch (provider.toLowerCase()) {
+			case "kakao" -> "/oauth2/authorization/kakao";
+			case "naver" -> "/oauth2/authorization/naver";
+			case "google" -> "/oauth2/authorization/google";
+			default -> throw new IllegalArgumentException("잘못된 소셜로그인입니다. 요청한 소셜로그인 : " + provider);
+		};
+		response.sendRedirect(redirectUrl);
 	}
 
 	@PostMapping("/api/onboarding")
